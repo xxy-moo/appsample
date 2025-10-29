2900a32ae8a0db1eef86b965b0cf8bb20d5da9fa
xxy-moo <xingxinyue1220@gmail.com>
2025-10-29 19:15:22 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('test')
+st.write('Hello')
+name=st.text_input('name?)
+if st.button('HELLO'):
+    st.success(f'안녕하세요,{name}님!')               
+                   
