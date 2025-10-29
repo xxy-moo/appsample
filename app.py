a0a6bf8574971f910c395fcbd73775850b16eed5
xxy-moo <xingxinyue1220@gmail.com>
2025-10-29 19:17:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title('test')
 st.write('Hello')
-name=st.text_input('name?)
+name=st.text_input('name?')
 if st.button('HELLO'):
     st.success(f'안녕하세요,{name}님!')               
                    
+
